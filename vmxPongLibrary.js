377742c94e58ce89db94409370f8ade1ae041c7e
gdoteof <geoff@vmx.ai>
2013-11-12 16:14:03 -0500

    fast commit

--- vmxPongLibrary.js
+++ vmxPongLibrary.js
@@ -142,6 +142,7 @@ function draw() {
   var right_pos = vmxApi(right_model).getSmooth();
   paddleRightY = right_pos.y;
   paddleLeftY  =  left_pos.y;
+  debugger;
   clear();
   circle(x, y, radius);
 
